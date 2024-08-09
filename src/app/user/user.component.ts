28efdbc24d23b9ef73c0cd08723a064ea06dd113
Hoàng Phương <101681993+HoangPhuong22@users.noreply.github.com>
2024-08-09 15:38:33 +0700

    Extracting values with two-way-binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,14 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+  
   onSubmit() {
     console.log('Form Submitted!');
+    console.log(this.enteredInitialInvestment);
   }
 }
